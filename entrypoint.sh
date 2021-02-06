bd6ec6f2a38209f7037ec0ac9d785b0d5a0b94de
= <=>
2021-02-06 14:43:55 -0500

    Correction for path at migrations

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,18 +5,19 @@ pipenv install --system
 pipenv install --dev --system
 export OLD_VERSION="$(alembic current | awk '{print $1}' )"
 export NEW_VERSION="$(alembic heads | awk '{print $1}')"
+export PYTHONPATH=app/
 echo "$OLD_VERSION ------- $NEW_VERSION"
 
 if [ "$OLD_VERSION" = "" ];
   then
     echo "FIRST MIGRATION"
-    alembic upgrade "$NEW_VERSION" --sql > migrations/flyway/db/Vbo_"$NEW_VERSION".sql
+    alembic upgrade "$NEW_VERSION" --sql > app/migrations/flyway/db/Vbo_"$NEW_VERSION".sql
 
 else
   if [ "$OLD_VERSION" = "$NEW_VERSION" ];
   then
     echo "NO MIGRATIONS"
   else
-    alembic upgrade "$OLD_VERSION":"$NEW_VERSION"  --sql > migrations/flyway/db/Vbo_"$OLD_VERSION"_"$NEW_VERSION".sql
+    alembic upgrade "$OLD_VERSION":"$NEW_VERSION"  --sql > app/migrations/flyway/db/Vbo_"$OLD_VERSION"_"$NEW_VERSION".sql
   fi
 fi
