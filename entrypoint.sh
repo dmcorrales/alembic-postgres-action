46e0c64fabda7eb20eef90f13fe0bb0ffb266996
= <=>
2021-02-08 09:57:00 -0500

    Dir for flyway -> Sql output

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,13 +12,13 @@ echo "$OLD_VERSION ------- $NEW_VERSION"
 if [ "$OLD_VERSION" = "" ];
   then
     echo "FIRST MIGRATION"
-    alembic upgrade "$NEW_VERSION" --sql > flyway/db/Vbo_"$NEW_VERSION".sql
+    alembic upgrade "$NEW_VERSION" --sql > flyway/sql/Vbo_"$NEW_VERSION".sql
 
 else
   if [ "$OLD_VERSION" = "$NEW_VERSION" ];
   then
     echo "NO MIGRATIONS"
   else
-    alembic upgrade "$OLD_VERSION":"$NEW_VERSION"  --sql > flyway/db/Vbo_"$OLD_VERSION"_"$NEW_VERSION".sql
+    alembic upgrade "$OLD_VERSION":"$NEW_VERSION"  --sql > flyway/sql/Vbo_"$OLD_VERSION"_"$NEW_VERSION".sql
   fi
 fi
